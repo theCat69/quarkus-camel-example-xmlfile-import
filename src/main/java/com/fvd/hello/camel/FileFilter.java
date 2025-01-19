f0e309cedb1728820d02aaeb74da13e0110c6caa
Felix Vadcard <vadcard.felix@gmail.com>
2025-01-19 13:02:35 +0100

    SuppressWarning raw use of GenericFileFilter

--- src/main/java/com/fvd/hello/camel/FileFilter.java
+++ src/main/java/com/fvd/hello/camel/FileFilter.java
@@ -14,6 +14,7 @@ import java.util.TimeZone;
 @ApplicationScoped
 @Named
 @Slf4j
+@SuppressWarnings("rawtypes")
 public class FileFilter implements GenericFileFilter {
 
   @ConfigProperty(name = "filter.lastmodified.seconds.beforeintegrate")
